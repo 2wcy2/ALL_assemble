f8af111c5a519d235d58ae2de2f953451687cd56
wangc-501 <wangchengyu819@sina.com>
2026-04-27 13:05:23 +0800

    天启通信存活

--- app/tasks/communication.c
+++ app/tasks/communication.c
@@ -2,22 +2,35 @@
 #include "cmsis_os2.h"
 #include "usart.h"
 #include "main.h"
-char message[1000];
-int tianqi_flag = 0;
+char message1[50], message2[50];
+int rx_varible_flag=0,rxcplt_flag=0;
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
-    tianqi_flag = 1;
-    HAL_UART_Transmit(&test_uart,message,strlen(message),HAL_MAX_DELAY);
-    HAL_UARTEx_ReceiveToIdle_DMA(&TIANQI_UART,message,1000);
+    if (rx_varible_flag==1) {
+        rxcplt_flag = 1;
+        HAL_UARTEx_ReceiveToIdle_DMA(&TIANQI_UART,message2,50);
+        rx_varible_flag=2;
+    }
+    else if (rx_varible_flag==2) {
+        rxcplt_flag = 2;
+        HAL_UARTEx_ReceiveToIdle_DMA(&TIANQI_UART,message1,50);
+        rx_varible_flag=1;
+    }
 }
 void Start_communication_task(void *argument) {
-
-    HAL_UARTEx_ReceiveToIdle_DMA(&TIANQI_UART,message,1000);
+    char trans_data[]="AT+CSQ?\r\n";
+    HAL_UARTEx_ReceiveToIdle_DMA(&TIANQI_UART,message1,50);
+    rx_varible_flag=1;
     osDelay(200);
-    // HAL_UART_Transmit(&TIANQI_UART,"ATE1\r\n",9,HAL_MAX_DELAY);
-    // osDelay(1000);
-    HAL_UART_Transmit(&TIANQI_UART,"AT+CLAC?\r\n",9,HAL_MAX_DELAY);
-    osDelay(1000);
+     HAL_UART_Transmit(&TIANQI_UART,trans_data,strlen(trans_data),HAL_MAX_DELAY);
     while (1) {
-        osDelay(100);
+        if (rxcplt_flag==2) {
+            HAL_UART_Transmit(&test_uart,message2,strlen(message2),HAL_MAX_DELAY);
+            rxcplt_flag=0;
+        }
+        else if (rxcplt_flag==1) {
+            HAL_UART_Transmit(&test_uart,message1,strlen(message1),HAL_MAX_DELAY);
+            rxcplt_flag=0;
+        }
+        //osDelay(1);
     }
 }
