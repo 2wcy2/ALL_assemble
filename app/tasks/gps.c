8b250da4002ed5f2c2e4d7d830847ec4dbc5a786
wangc <wangchengyu819@sina.com>
2026-05-17 16:54:52 +0800

    gps测试 gps不定位

--- app/tasks/gps.c
+++ app/tasks/gps.c
@@ -4,15 +4,17 @@
 #include "gpio.h"
 #include "usart.h"
 int rx_gps_flag=0;
-char message[500];
+char message[700];
 void Start_gps_locate_tast(void *argument) {
     HAL_GPIO_WritePin(GPS_PWR_GPIO_Port, GPS_PWR_Pin, GPIO_PIN_RESET);
-    HAL_UARTEx_ReceiveToIdle_DMA(&GPS_UART,message,500);
+    HAL_UARTEx_ReceiveToIdle_DMA(&GPS_UART,message,700);
     while (1) {
         if (rx_gps_flag==1) {
+
             rx_gps_flag=0;
             HAL_UART_Transmit(&test_uart,message,strlen(message),1000);
-            HAL_UARTEx_ReceiveToIdle_DMA(&GPS_UART,message,500);
+            memset(message,0,sizeof(message));
+            HAL_UARTEx_ReceiveToIdle_DMA(&GPS_UART,message,700);
         }
     }
 }
